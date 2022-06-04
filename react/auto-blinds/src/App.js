974b7c586611b912c23004c6c78edb54f8d8d20a
Ian <ian5000000@yahoo.com>
2022-06-03 23:24:09 -0400

    good enough for tonight. Gets the idea across.

--- react/auto-blinds/src/App.js
+++ react/auto-blinds/src/App.js
@@ -22,6 +22,26 @@ function App() {
         <Input stepsInput1 className="input-label" placeholder="0" />
         <Progress className="potRotation1" type="dashboard" percent={50} showInfo={false} size='small' />
       </Space>
+      <Space direction='vertical' align='center' size="large">
+        <Space direction='Horizontal' align='center' size="large" >
+          <TimePicker className="timePicker1" format="HH:mm" />
+          <TimePicker className="timePicker1" format="HH:mm" />
+          <TimePicker className="timePicker1" format="HH:mm" />
+          <TimePicker className="timePicker1" format="HH:mm" />
+          <TimePicker className="timePicker1" format="HH:mm" />
+          <TimePicker className="timePicker1" format="HH:mm" />
+          <TimePicker className="timePicker1" format="HH:mm" />
+        </Space>
+        <Space direction='Horizontal' align='center' size="large" >
+          <TimePicker className="timePicker1" format="HH:mm" />
+          <TimePicker className="timePicker1" format="HH:mm" />
+          <TimePicker className="timePicker1" format="HH:mm" />
+          <TimePicker className="timePicker1" format="HH:mm" />
+          <TimePicker className="timePicker1" format="HH:mm" />
+          <TimePicker className="timePicker1" format="HH:mm" />
+          <TimePicker className="timePicker1" format="HH:mm" />
+        </Space>
+        </Space>
     </div>
 
     <div className="motor2">
